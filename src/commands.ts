72604a00434efc2f68fbbd3239d25a30c4d6ec32
mat <github@matdoes.dev>
2021-05-14 10:33:24 -0500

    move some stuff into commands.ts

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,15 @@
 import Brigadier, { ArgumentType, StringReader, Suggestions } from 'node-brigadier'
-import { client, recentUsers } from '.'
+import { client } from '.'
 import Discord from 'discord.js'
 
+// if you want to change the bot's prefix, change this value
+const PREFIX = '!'
+
 export const dispatcher: Brigadier.CommandDispatcher<Discord.Message> = new Brigadier.CommandDispatcher()
+
+/** User ids mapped to the last time they sent a message */
+export let recentUsers: { [ key: string ]: number } = {}
+
 
 export class UserArgument implements ArgumentType<Discord.User> {
 	id: string
@@ -86,6 +93,48 @@ export class UserArgument implements ArgumentType<Discord.User> {
 		]
 	}
 }
+
+client.on('message', async(message: Discord.Message) => {
+	recentUsers[message.author.id] = Date.now()
+
+	// the author can't be a bot
+	if (message.author.bot) return
+	// the message has to start with the prefix
+	else if (!message.content.startsWith(PREFIX)) return
+
+	const content = message.content.slice(PREFIX.length).trim()
+	const parsedCommand = dispatcher.parse(content, message)
+	try {
+		await dispatcher.execute(parsedCommand)
+	} catch (ex) {
+		if (ex.getMessage) {
+			const errorMessage: string = ex.getMessage()
+
+			// don't care about these errors
+			if (errorMessage.startsWith('Unknown command at position 0: '))
+				return
+
+			console.error(errorMessage)
+			await message.channel.send(errorMessage)
+		} else {
+			console.error(ex)
+		}
+	}
+})
+
+/** Remove users that haven't talked for an hour from `recentUsers` */
+function flushRecentUsers() {
+	const cutoff = Date.now() - (60 * 60 * 1000)
+	for (const [ userId, lastMessageDate ] of Object.entries(recentUsers)) {
+		if (lastMessageDate < cutoff) {
+			delete recentUsers[userId]
+		}
+	}
+}
+
+// flush recent users every minute
+setInterval(flushRecentUsers, 60 * 1000)
+
 
 // import all the commands here, this must be updated every time you add a new command
 import './commands/ping'
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,12 @@
 require('dotenv').config()
 import Discord from 'discord.js'
-import * as commands from './commands'
-
-// if you want to change the bot's prefix, change this value
-const PREFIX = '!'
-
-
-import './database'
 
 export const client: Discord.Client = new Discord.Client({
 	intents: Discord.Intents.ALL &~ Discord.Intents.FLAGS.GUILD_PRESENCES
 })
 
-/** User ids mapped to the last time they sent a message */
-export let recentUsers: { [ key: string ]: number } = {}
-
-
-client.on('message', async(message: Discord.Message) => {
-	recentUsers[message.author.id] = Date.now()
-
-	// the author can't be a bot
-	if (message.author.bot) return
-	// the message has to start with the prefix
-	else if (!message.content.startsWith(PREFIX)) return
-
-	const content = message.content.slice(PREFIX.length).trim()
-	const parsedCommand = commands.dispatcher.parse(content, message)
-	try {
-		await commands.dispatcher.execute(parsedCommand)
-	} catch (ex) {
-		if (ex.getMessage) {
-			const errorMessage: string = ex.getMessage()
-
-			// don't care about these errors
-			if (errorMessage.startsWith('Unknown command at position 0: '))
-				return
-
-			console.error(errorMessage)
-			await message.channel.send(errorMessage)
-		} else {
-			console.error(ex)
-		}
-	}
-})
-
-/** Remove users that haven't talked for an hour from `recentUsers` */
-function flushRecentUsers() {
-	const cutoff = Date.now() - (60 * 60 * 1000)
-	for (const [ userId, lastMessageDate ] of Object.entries(recentUsers)) {
-		if (lastMessageDate < cutoff) {
-			delete recentUsers[userId]
-		}
-	}
-}
-
-// flush recent users every minute
-setInterval(flushRecentUsers, 60 * 1000)
+import './database'
+import './commands'
 
 client.on('ready', () => {
 	console.log('ready')
